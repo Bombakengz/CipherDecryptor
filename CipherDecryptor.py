3040ccab3cd9a38e812480bd49bebe6074750f0a
Bombussy <166336056+Bombakengz@users.noreply.github.com>
2024-09-25 21:27:59 -0300

    CipherDecryptor.py

--- CipherDecryptor.py
+++ CipherDecryptor.py
@@ -10,7 +10,7 @@ decrypt_text = ""
 
 for letter in encrypted_text:
     if letter in chars:
-        index = chars.index(letra)
+        index = chars.index(letter)
         decrypt_text += chars[(index - displacement) % len(chars)]  #Cipher Algorithm
     else:
         decrypt_text += letter
