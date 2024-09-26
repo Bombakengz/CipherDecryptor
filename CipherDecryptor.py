84256ec4492443567b03d16e009fd8431365f2f1
Bombussy <166336056+Bombakengz@users.noreply.github.com>
2024-09-25 21:14:24 -0300

    Add files via upload
    This is a simple Cipher Decryptor I made for a Cibersecurity Tournament

---
+++ CipherDecryptor.py
@@ -0,0 +1,20 @@
+import string
+
+chars = "abcdefghijklmnñopqrstuvwxyz"
+displacement = 4
+
+#Decrypt
+encrypted_text = input("Input your Crypted message to Decrypt: ")
+print("")
+decrypt_text = ""
+
+for letter in encrypted_text:
+    if letter in chars:
+        index = chars.index(letra)
+        decrypt_text += chars[(index - displacement) % len(chars)]  #Cipher Algorithm
+    else:
+        decrypt_text += letter
+
+print("Crypted Message: " + encrypted_text)
+print("")
+print("Decrypted Message: " + decrypt_text)
